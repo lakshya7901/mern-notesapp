e86aaecce072f93dca0449a9bf1fcd467f38589f
Lakshya <lakshya7901@gmail.com>
2025-09-25 12:15:49 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,30 @@
-import experss from "express";
+import express from "express";
 import cors from 'cors'
 import dotenv from 'dotenv';
+import path from 'path'
 
 import { connectDB } from "../config/db.js";
 
 import notesRouter from "./routes/notesRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
-const app = experss()
+const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(
-    cors({
-        // origin:'*',    
-        origin:['http://localhost:5173']
-    })
-)
-app.use(experss.json());
+if(process.env.NODE_ENV !== 'production'){
+    app.use(
+        cors({
+            // origin:'*',    
+            origin:['http://localhost:5173']
+        })
+    );
+}
+
+app.use(express.json());
 app.use(rateLimiter);
 // app.use((req,res,next)=>{
 //     console.log(`Request method is ${req.method} &  Request URL is ${req.url}`)
@@ -27,6 +32,14 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRouter);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+
+    app.get(/.*/, (req,res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
