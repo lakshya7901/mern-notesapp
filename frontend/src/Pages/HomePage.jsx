adf372de493a29e158c225ec517f4b0268747643
Lakshya <lakshya7901@gmail.com>
2025-09-25 13:30:29 +0530

    ready to deploy

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -7,7 +7,7 @@ import toast from 'react-hot-toast'
 import NotesNotFound from '../Components/NotesNotFound'
 
 const HomePage = () => {
-  const [rateLimit, setrateLimit] = useState(true);
+  const [rateLimit, setrateLimit] = useState(false);
   const [notes, setnotes] = useState([]);
   const [loading, setloading] = useState(true);
 
@@ -36,7 +36,7 @@ const HomePage = () => {
   return (
     <div className='min-h-screen'>
       <Navbar />
-      {rateLimit && <RateLimitedUi />}
+      {rateLimit }
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'>
           Loading Notes...
