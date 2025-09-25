06a15574cea87458db23e0b7ff784218bb7f517e
Lakshya <lakshya7901@gmail.com>
2025-09-25 13:34:27 +0530

    ready to deploy

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -36,7 +36,7 @@ const HomePage = () => {
   return (
     <div className='min-h-screen'>
       <Navbar />
-      {rateLimit }
+      {rateLimit && <RateLimitedUi />}
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'>
           Loading Notes...
